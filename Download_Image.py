1355ec82ed48e25c7a8f74a62468ce16a7e7a015
i-ensarkok <iensarkok@gmail.com>
2022-09-26 23:38:47 +0300

    commit message

---
+++ Download_Image.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("Download Image")  
+
+# Creating a label widget
+myLabel = Label(root, text = "Search Image Name: ").grid(row = 0, column= 0, sticky="e")
+# Showing it into a screen
+#myLabel.pack()
+
+root.mainloop()
+
