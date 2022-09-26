f071dff27ca8d2e3980973ded50ba34db3c23899
i-ensarkok <iensarkok@gmail.com>
2022-09-27 00:10:53 +0300

    “commit_message”

--- Download_Image.py
+++ Download_Image.py
@@ -1,12 +1,19 @@
 from tkinter import *
+import tkinter
 
 root = Tk()
 root.title("Download Image")  
 
-# Creating a label widget
-myLabel = Label(root, text = "Search Image Name: ").grid(row = 0, column= 0, sticky="e")
-# Showing it into a screen
-#myLabel.pack()
+searchText = StringVar()
+def click():
+    print(searchText.get())
+
+# Creating search part
+searchLabel = Label(root, text = "Search Image Name: ").grid(row = 0, column= 0, sticky="w")
+
+searchEntry = Entry(root, width= 30, textvariable= searchText).grid(row=0, column= 1, padx= 2, pady= 2, sticky= "we", columnspan=9)
+searchButton = Button(root, text= "Search", command = click).grid(row= 1, column=9, sticky= "we" )
+
 
 root.mainloop()
 
