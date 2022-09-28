e1e964374a5b3a17673fca9f54c1f8c51b353e64
i-ensarkok <iensarkok@gmail.com>
2022-09-29 00:20:49 +0300

    commit_message

--- Download_Image.py
+++ Download_Image.py
@@ -1,19 +1,41 @@
 from tkinter import *
-import tkinter
+from bing_image_downloader import downloader
 
+
+# Main window specifications
 root = Tk()
-root.title("Download Image")  
+root.geometry("500x240")
+root.title("Download Image")
+root.resizable(0,0)  
 
+# Variables
 searchText = StringVar()
+numImgText = StringVar()
+outputDirText = StringVar()
+
+def download():
+    downloader.download(searchText.get(), limit= int(numImgText.get()), output_dir= outputDirText.get(), 
+    adult_filter_off=True, force_replace= False, timeout=60, verbose=True)
+
+
 def click():
-    print(searchText.get())
+    if len(searchText.get()) != 0 :
+        print(searchText.get())
+        download()
+    else:
+        print("Entry is null")
 
-# Creating search part
+# Creating search block
 searchLabel = Label(root, text = "Search Image Name: ").grid(row = 0, column= 0, sticky="w")
+searchEntry = Entry(root, width= 30, textvariable= searchText).grid(row=0, column= 1, padx= 2, pady= 2, sticky= "w", columnspan=9)
+searchButton = Button(root, text= "Search", command = click).grid(row= 3, column=9, sticky= "w" )
 
-searchEntry = Entry(root, width= 30, textvariable= searchText).grid(row=0, column= 1, padx= 2, pady= 2, sticky= "we", columnspan=9)
-searchButton = Button(root, text= "Search", command = click).grid(row= 1, column=9, sticky= "we" )
+# Creating image block
+numImgLabel = Label(root, text= "Number of images to download: ").grid(row= 1, column= 0, sticky= "w")
+numImgEntry = Entry(root, width= 10, textvariable= numImgText).grid(row= 1, column= 1, padx= 2, pady= 2, sticky="w") 
 
-
+# Creating output direction block
+outputDirLabel = Label(root, text= "Output direction folder name: ").grid(row= 2, column= 0, sticky= "w")
+outputDirEntry = Entry(root, width= 15, textvariable= outputDirText).grid(row= 2, column= 1, padx= 2, pady= 2, sticky="w") 
 root.mainloop()
 
